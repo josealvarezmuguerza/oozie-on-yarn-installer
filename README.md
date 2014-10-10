5e52e68436dc24296f45ec567b4a2054dc3b83d0
josealvarezmuguerza <josealvarezmuguerza@gmail.com>
2014-10-10 09:19:33 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 oozie-at-yarn-installer
 =======================
+
+single script for installing Oozie Workflow Scheduler in local based on Hadoop Yarn.
+
+###Requirements
+
+- Hadoop Yarn (running)
+- JDK 6+
+
+![image](http://oozie.apache.org/images/oozie_200x.png)
+
+###HOW-TO
+
+```java
+git clone git@github.com:josealvarezmuguerza/oozie-at-yarn-installer.git
+cd oozie-at-yarn-installer
+```
+
+Open install-oozie.sh in your preferred editor and set the following parameters as desired:
+
+```java
+##Oozie version you want to install
+OOZIE_VERSION=3.3.2
+##Installed hadoop version 
+MVN_PROFILE_HADOOP_VERSION=hadoop-23
+##How Oozie calls your above Hadoop version
+OOZIE_HADOOP_VERSION=hadoop-2
+##Your hadoop user
+HADOOP_USER=jose
+##Where you want to install Oozie
+export OOZIE_HOME=/home/jose/tools/oozie-$OOZIE_VERSION
+```
+
+The above configuration belongs to Oozie v3.3.2 against Hadoop 2.3, and my hadoop's user is 'jose'. You should be able to set other versions here, but always over Hadoop Yarn. 
+
+This script will let your Oozie up & running, so, after it runs you can open it at http://localhost:11000/oozie
+
+enjoy it!
+
+##License
+Apache License, Version 2.0 http://www.apache.org/licenses/LICENSE-2.0
---
+++ install-oozie.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+##This script aims to easily install Oozie runing in local hadoop cluster. 
+# Prerequisites: Hadoop 2 up & running
+#                JDK 6+
+
+## download and extract Oozie 3.3.2 (4.0.0 doesn't build for me)
+
+##Define folders and oozie version
+
+##customize these 5 values
+##Oozie version you want to install
+OOZIE_VERSION=3.3.2
+##Installed hadoop version 
+MVN_PROFILE_HADOOP_VERSION=hadoop-23
+##How Oozie calls your above Hadoop version
+OOZIE_HADOOP_VERSION=hadoop-2
+##Your hadoop user
+HADOOP_USER=jose
+##Where you want to install Oozie
+export OOZIE_HOME=/home/jose/tools/oozie-$OOZIE_VERSION
+
+
+LOCAL_DIR=`pwd`
+DOWNLOAD_DIR=$LOCAL_DIR/oozie-talball
+BUILD_DIR=$DOWNLOAD_DIR/oozie-$OOZIE_VERSION
+
+
+##Download tarball from repo
+mkdir -p $BUILD_DIR
+cd $DOWNLOAD_DIR
+wget http://archive.apache.org/dist/oozie/$OOZIE_VERSION/oozie-$OOZIE_VERSION.tar.gz
+tar xzvf oozie-$OOZIE_VERSION.tar.gz
+
+cd $BUILD_DIR
+
+##compiling
+mvn -DskipTests=true -P $MVN_PROFILE_HADOOP_VERSION clean package assembly:single    
+
+##copy bin to OOZIE_HOME
+mkdir -p $OOZIE_HOME
+cp ${BUILD_DIR}/distro/target/oozie-$OOZIE_VERSION-distro.tar.gz $OOZIE_HOME
+cd $OOZIE_HOME
+tar xzvf $OOZIE_HOME/oozie-$OOZIE_VERSION-distro.tar.gz -C $OOZIE_HOME/
+mv  $OOZIE_HOME/oozie-$OOZIE_VERSION/* $OOZIE_HOME
+#this folder should be emplty here 
+rm -rf $OOZIE_HOME/oozie-$OOZIE_VERSION
+
+##Copy examples folder
+cp -r ${BUILD_DIR}/examples/target/oozie-examples-$OOZIE_VERSION-examples/* $OOZIE_HOME
+
+##Adding libraries
+mkdir $OOZIE_HOME/libext
+cd $OOZIE_HOME/libext
+wget http://extjs.com/deploy/ext-2.2.zip
+
+
+OOZIE_HADOOP_AUTH_VERSION=`xmllint --xpath "(//*[local-name()='hadoop.auth.version']/text())[1]" $BUILD_DIR/pom.xml`
+cp $BUILD_DIR/hadooplibs/$OOZIE_HADOOP_VERSION/target/hadooplibs/hadooplib-$OOZIE_HADOOP_AUTH_VERSION.oozie-$OOZIE_VERSION/* $OOZIE_HOME/libext
+
+##Prepare and deploy oozie war
+$OOZIE_HOME/bin/oozie-setup.sh prepare-war
+
+
+##edit core-site in hadoop 
+sed -i 's/<\/configuration>/\n <!-- oozie-->\n  <property>\n    <name>hadoop.proxyuser.'"$HADOOP_USER"'.hosts<\/name>\n    <value>localhost<\/value>\n  <\/property>\n\n  <property>\n    <name>hadoop.proxyuser.'"$HADOOP_USER"'.groups<\/name>\n    <value>'"$HADOOP_USER"'<\/value>\n  <\/property>\n \n<\/configuration>/'  $HADOOP_HOME/etc/hadoop/core-site.xml 
+
+
+# prepare DB - MySqlServer must be already in place
+cd $OOZIE_HOME/bin/
+$OOZIE_HOME/bin/ooziedb.sh create -sqlfile oozie.sql -run
+
+# Put required libraries to HDFS
+#HDFS=`xmllint --xpath "//configuration/property[name='fs.defaultFS']/value/text()" $HADOOP_HOME/etc/hadoop/core-site.xml`
+hdfs dfs -mkdir -p /user/$HADOOP_USER
+hdfs dfs -put ${BUILD_DIR}/sharelib/target/oozie-sharelib-$OOZIE_VERSION/share /user/$HADOOP_USER
+
+
+# Run Oozie
+bin/oozied.sh run
+bin/oozie admin -oozie http://localhost:11000/oozie -status
+
